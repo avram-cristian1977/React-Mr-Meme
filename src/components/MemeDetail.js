a6114b61121a0d02bb0892aa8f924a18afb6580f
avram-cristian1977 <asct1977ro@gmail.com>
2021-06-14 11:26:38 +0300

    fix details end point

--- src/components/MemeDetail.js
+++ src/components/MemeDetail.js
@@ -1,12 +1,38 @@
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import classes from '../pages/Search.module.css'
+
+import classes from './MemeDetail.module.css'
+import { useEffect, useState } from 'react'
 
 
-const MemeDetail = ({meme}) => {
+const MemeDetail = ({ match }) => {
 
-console.log("detail")
+    const [details, setDetails] = useState()
+
+    useEffect(() => {
+
+        async function fetchItem() {
+            try {
+
+                const response = await fetch(`
+                https://api.giphy.com/v1/gifs/${match.params.id}?api_key=L6nDPtw5VOh9KiBzRaWThIKfXWFVfNUf`)
+                const responseJson = await response.json()
+                console.log(responseJson.data);
+                setDetails(responseJson.data.images.preview.mp4)
+
+            } finally {
+
+            }
+
+        }
+        fetchItem()
+
+
+    }, [match.params.id])
+
+    console.log(match)
     return <>
-    meme details
+        <div className={classes.videoWrapper}>
+            <video src={details} autoPlay loop width="640" height="480" />
+        </div>
     </>
 
 }
